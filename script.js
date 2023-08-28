9e5c820a100b00ffc4230fa6203936e1f5c7dfde
Anna <anna.grygierczyk@wp.pl>
2023-08-28 18:08:02 +0200

    calculation code and AC and C functions added

--- script.js
+++ script.js
@@ -1,15 +1,103 @@
 const operatorButtons = document.querySelectorAll('.operatorBtn');
-const numberButtons = document.querySelectorAll('.numberBtn');
+const numberButtons = document.querySelectorAll(".numberBtn");
 const clearAllBtn = document.querySelector('.clearAllBtn');
 const deleteBtn = document.querySelector('.deleteBtn');
 const result = document.querySelector('.result');
-const previousInput = document.querySelector('.previous')
+const previousDisplay = document.querySelector('.previous');
+const currentDisplay = document.querySelector('.current');
 
-let displayValue;
+let operator = '';
+let previousNum = '';
+let currentNum = '';
 
-/*input display*/ 
+clearAllBtn.addEventListener('click', () => {
+    clearAll();
+})
 
+function clearAll(){
+    currentNum = '';
+    operator = '';
+    previousNum = '';
+    currentDisplay.textContent = currentNum;
+    previousDisplay.textContent = previousNum;
+}
 
+deleteBtn.addEventListener('click', () => {
+    deleteLast();
+})
+
+function deleteLast(){
+    if(currentNum.length === 1){
+        currentNum = ''
+        currentDisplay.textContent = currentNum;
+    }else{
+        currentNum = currentNum.substring(0, currentNum.length - 1);
+        currentDisplay.textContent = currentNum;
+    }
+}
+
+numberButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        handleNum(e.target.textContent)
+    })
+});
+
+function handleNum(number){
+    currentNum += number;
+    currentDisplay.textContent = currentNum;
+}
+
+operatorButtons.forEach((operator)=>{
+    operator.addEventListener('click', (e) =>{
+        handleOperator(e.target.textContent);
+    })
+})
+
+function handleOperator(operatorInput){
+
+    operate();
+    currentNum = currentDisplay.textContent;
+
+    operator = operatorInput;
+    previousNum = currentNum;
+    currentNum = '';
+
+    currentDisplay.textContent = currentNum;
+    previousDisplay.textContent = previousNum + operator;
+
+    
+}
+
+result.addEventListener('click', ()=>{
+    previousDisplay.textContent = previousNum + operator + currentNum;
+    operate();
+    currentNum = currentDisplay.textContent;
+
+})
+
+function operate(){
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+    
+    if(operator==='+'){
+        let added = add(previousNum, currentNum);
+        currentDisplay.textContent = added;
+    
+    }else if(operator==='-'){
+        let substracted = substract(previousNum, currentNum);
+        currentDisplay.textContent = substracted;
+   
+    }else if(operator==='x'){
+        let mulitplied = multiply(previousNum, currentNum);
+        currentDisplay.textContent = mulitplied;
+     
+    }else if(operator==='÷'){
+        let divided = divide(previousNum, currentNum);
+        currentDisplay.textContent = divided;
+ 
+    }
+
+}
 
 
 function add(a,b){
@@ -26,22 +114,6 @@ function multiply(a,b){
 
 function divide(a,b){
     return a/b;
-}
-
-let num1;
-let num2;
-let operator;
-
-function operate(num1, num2, operator){
-    if(operator=='+'){
-        return add(num1, num2)
-    }else if(operator=='-'){
-        return substract(num1, num2)
-    }else if(operator=='*'){
-        return multiply(num1, num2)
-    }else if(operator=='/'){
-        return divide(num1, num2)
-    }
 }
 
 
